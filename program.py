59efec5109930861467daee083e070ae2cc0c478
Айдар <karimmovaydar013@gmail.com>
2021-10-24 23:17:23 +0300

    загрузка main дизайна

--- program.py
+++ program.py
@@ -1,7 +1,37 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import sys
+
+from PyQt5.QtWidgets import *
+
+from PyQt5.uic import loadUiType
+Form, Window = loadUiType('main.ui')
+
+
+class MainApp(QMainWindow, Form):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+# import sys
+#
+# from PyQt5 import uic  # Импортируем uic
+# from PyQt5.QtWidgets import QApplication, QMainWindow
+#
+#
+# class MyWidget(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         uic.loadUi('main.ui', self)
+#
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = MyWidget()
+#     ex.show()
+#     sys.exit(app.exec_())
