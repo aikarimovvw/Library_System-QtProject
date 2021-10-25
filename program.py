ca53a7dd3f68651eeefd3f6bb4b5abba2f88188e
Айдар <karimmovaydar013@gmail.com>
2021-10-25 16:56:06 +0300

    работает tbl_wid, main_buttons

--- program.py
+++ program.py
@@ -1,37 +1,34 @@
 import sys
 
-from PyQt5.QtWidgets import *
-
-from PyQt5.uic import loadUiType
-Form, Window = loadUiType('main.ui')
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MainApp(QMainWindow, Form):
+class MyWidget(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
-        self.setupUi(self)
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.btn_main_oper.clicked.connect(self.Open_Operations)
+        self.btn_main_clients.clicked.connect(self.Open_Clients)
+        self.btn_main_empl.clicked.connect(self.Open_Employee)
+        self.btn_main_books.clicked.connect(self.Open_Books)
+        self.tbl_wdgt.tabBar().setVisible(False)
+
+    def Open_Operations(self):
+        self.tbl_wdgt.setCurrentIndex(0)
+
+    def Open_Books(self):
+        self.tbl_wdgt.setCurrentIndex(1)
+
+    def Open_Clients(self):
+        self.tbl_wdgt.setCurrentIndex(2)
+
+    def Open_Employee(self):
+        self.tbl_wdgt.setCurrentIndex(3)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainApp()
-    window.show()
-    app.exec_()
-
-# import sys
-#
-# from PyQt5 import uic  # Импортируем uic
-# from PyQt5.QtWidgets import QApplication, QMainWindow
-#
-#
-# class MyWidget(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#         uic.loadUi('main.ui', self)
-#
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     ex = MyWidget()
-#     ex.show()
-#     sys.exit(app.exec_())
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
