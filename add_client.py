29b0ce84c0582ab99a13a3567e451a7a73a4e9aa
Айдар <karimmovaydar013@gmail.com>
2021-11-04 23:25:03 +0300

    file add_client

--- add_client.py
+++ add_client.py
@@ -1,11 +1,12 @@
 import sys
-import sqlite3
 from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
-from PyQt5.QtWidgets import QFileDialog
 from PyQt5 import uic
 import functions_for_add
+import library_db
+from CONST_VALUES import *
 
 
+# окно для добавления читателя
 class AddClient(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -14,40 +15,21 @@ class AddClient(QMainWindow):
 
     def save_client(self):
         name = self.ledit_add_name_c.text()
-        phone_num_client = self.ledit_add_num_c.text()
+        num_client = self.ledit_add_num_c.text()
         mail_client = self.ledit_add_mail_c.text()
         address_client = self.ledit_add_address.text()
         b_date = self.clndr_wdgt_add_date.selectedDate().toString("yyyy-MM-dd")
+        if functions_for_add.check_clients(name, num_client, mail_client, address_client, b_date) is False:
+            return self.statusBar().showMessage('Введите корректные данные')
 
-        name_check = functions_for_add.check_len(name)
-        number_check = functions_for_add.check_len(phone_num_client)
-        mail_check = functions_for_add.check_len(mail_client)
-        address_check = functions_for_add.check_len(address_client)
-        year_check = functions_for_add.check_date(b_date)
-
-        if all([name_check, number_check, mail_check, address_check]):
-            if year_check is False:
-                self.statusBar().showMessage('Введите корректную дату рождения')
-                return None
-        else:
-            self.statusBar().showMessage('Пустая строка, введите заново')
-            return None
-
-        con = sqlite3.connect("db_lib.sqlite")
-        cur = con.cursor()
-        res_check_client = cur.execute("""SELECT * FROM clients
-                WHERE client_number =?""", (phone_num_client,)).fetchone()
+        res_check_client = library_db.select_one_with_aspect(CLIENTS, CLIENT_NUMBER, num_client, '*')
         if res_check_client is None:
-            cur.execute("""INSERT INTO clients(client_name, client_number, client_mail, client_adres, client_date) 
-                    VALUES(?, ?, ?, ?, ?)""",
-                        (name, phone_num_client, mail_client, address_client, b_date))
-            con.commit()
-            con.close()
+            library_db.insert_for_clients(name, num_client, mail_client, address_client, b_date)
             self.statusBar().setStyleSheet("color : green")
-            self.statusBar().showMessage('Книга успешно добавлена!')
+            self.statusBar().showMessage('Клиент успешно добавлен!')
             self.close()
         else:
-            self.statusBar().showMessage('Пользователь с таким номером существует')
+            self.statusBar().showMessage('Клиент с таким номером существует')
 
     def clear_all(self):
         self.statusBar().showMessage('')
