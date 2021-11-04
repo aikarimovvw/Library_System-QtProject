b1abff0373ddc77e6511bbe9298e56122b7e27e6
Айдар <karimmovaydar013@gmail.com>
2021-11-04 23:27:37 +0300

    file authorization

--- authorization.py
+++ authorization.py
@@ -4,7 +4,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 from registration import RegEmployee
 from home import HomeScreen
-import sqlite3
+import library_db
+from CONST_VALUES import *
 
 
 class AddClient(QMainWindow):
@@ -20,22 +21,19 @@ class AddClient(QMainWindow):
         self.reg_empl_wind.show()
 
     def enter(self):
-        con = sqlite3.connect('db_lib.sqlite')
-        cur = con.cursor()
-        login = self.ledit_login_e_enter.text()
-        res = cur.execute("""SELECT * FROM Employee WHERE login = ?""", (login,)).fetchone()
-        if res is None:
+        login_inp = self.ledit_login_e_enter.text()
+        password_db = library_db.select_one_with_aspect(EMPLOYEE, LOGIN, login_inp, PASSWORD)
+        if password_db is None:
             self.statusBar().showMessage('Неверный логин')
         else:
             password = self.ledit_pass_e_enter.text()
-            password_db = str(res[1])
+            password_db = password_db[0]
             if password != password_db:
                 self.statusBar().showMessage('Неправильный пароль')
             else:
                 self.statusBar().setStyleSheet("color : green")
                 self.statusBar().showMessage('Успешно')
                 self.transition_home_wind.show()
-                con.close()
                 self.close()
 
 
